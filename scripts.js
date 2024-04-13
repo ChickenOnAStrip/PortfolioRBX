12955b2b866bb60b76e9dcd358718307ad4318fb
ChickenStrxp <84151164+ChickenOnAStrip@users.noreply.github.com>
2024-04-13 13:09:24 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('toggle-mode').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-    document.querySelectorAll('.portfolio-item').forEach(function(item) {
-        item.classList.toggle('dark-mode');
-    });
-});
